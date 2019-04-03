aab61b598c6db0e9d5c691384f1bb149b1835086
MateuszMilewski1997 <milewskimateusz28@gmail.com>
2019-04-03 09:06:52 +0200

    add column Comment to model

--- fakultet/Models/Documents.cs
+++ fakultet/Models/Documents.cs
@@ -2,15 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace fakultet.Models
 {
     public class DocumentStatusCOM
     {
 
-        [Key] [Column(TypeName = "int")] public int? Id { get; set; }
+         [Key] [Column(TypeName = "int")] public int? Id { get; set; }
          [Column(TypeName = "varchar(60)")] public string Name_Doc { get; set; }
          [Column(TypeName = "varchar(60)")] public string User_Mail { get; set; }
          [Column(TypeName = "varchar(60)")] public string Type_document { get; set; }
@@ -19,6 +17,7 @@ namespace fakultet.Models
          //[Column(TypeName = "DateTime")] public DateTime Send_Date { get; set; }
          [Column(TypeName = "varchar(500)")] public string Document_Description { get; set; }
          [Column(TypeName = "varchar(60)")] public string Status { get; set; }
+         [Column(TypeName = "nvarchar(MAX)")] public string Comment { get; set; }
 
         //  add table with comments to documents 
 
